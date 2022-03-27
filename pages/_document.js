611ba2326206ae73d11aba4fd499bcdf7c28f463
Igor Hoffmann Becker <igorhoffmannbecker10@gmail.com>
2022-03-27 14:35:48 -0300

    test 07

--- pages/_document.js
+++ pages/_document.js
@@ -15,6 +15,7 @@ class MyDocument extends Document {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&family=Source+Sans+Pro:wght@300;400;600;700&display=swap" rel="stylesheet" />
+        {/* Global site tag (gtag.js) - Google Analytics */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-MTB3BGJQCG"></script>
         <script>
         window.dataLayer = window.dataLayer || [];
