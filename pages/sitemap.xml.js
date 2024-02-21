e12b7d2222b46bd91cdc0ce77fcaa7ed4922633e
Igor Hoffmann Becker <igorhoffmannbecker10@gmail.com>
2024-02-21 17:43:59 -0300

    fix sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,7 +1,7 @@
 import getAllProducts from "../lib/getAllProducts";
 
 function generateSiteMap(produtos) {
-    const baseUrl = "https://www.ovosdecodorna.com.br";
+    const baseUrl = "https://www.ovosdecodorna.com";
     console.log(produtos)
     return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
